d66562f01eb7e4b8d03008a9c4fc49b9c9f74c04
Charlie Lor <lorcharlie@gmail.com>
2016-10-09 16:00:24 -0500

    Removed eslint-google, added some more hmong leeg tests and updated some syntax

--- tests/spellchecker.js
+++ tests/spellchecker.js
@@ -1,148 +1,153 @@
 'use strict';
 
-var should = require('should');
 var hmongspellchecker = require('../lib/spellchecker');
 
-describe('Invalid Hmong words', function () {
-    it('empty string', function () {
-        hmongspellchecker.check('', function (result) {
+describe('Invalid Hmong words', function() {
+    it('empty string', function() {
+        hmongspellchecker.check('', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('null', function () {
-        hmongspellchecker.check(null, function (result) {
-            result.should.equal(false);
-        });
-    })
-
-    it('only spaces', function () {
-        hmongspellchecker.check('   ', function (result) {
+    it('null', function() {
+        hmongspellchecker.check(null, function(result) {
             result.should.equal(false);
         });
     });
 
-    it('only special characters', function () {
-        hmongspellchecker.check('!@#$%', function (result) {
+    it('only spaces', function() {
+        hmongspellchecker.check('   ', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('only integers', function () {
-        hmongspellchecker.check(12345, function (result) {
+    it('only special characters', function() {
+        hmongspellchecker.check('!@#$%', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('only decimal', function () {
-        hmongspellchecker.check(12.345, function (result) {
+    it('only integers', function() {
+        hmongspellchecker.check(12345, function(result) {
+            result.should.equal(false);
+        });
+    });
+
+    it('only decimal', function() {
+        hmongspellchecker.check(12.345, function(result) {
             result.should.equal(false);
         });
     });
 
     it('only unicode', function() {
-        hmongspellchecker.check('\u03A9', function (result) {
-            result.should.equal(false);
-        });
-    })
-
-    it('invalid word - "Stuffed"', function () {
-        hmongspellchecker.check('Stuffed', function (result) {
+        hmongspellchecker.check('\u03A9', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "Chicken"', function () {
-        hmongspellchecker.check('Chicken', function (result) {
+    it('invalid word - "Stuffed"', function() {
+        hmongspellchecker.check('Stuffed', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "Wings"', function () {
-        hmongspellchecker.check('Wings', function (result) {
+    it('invalid word - "Chicken"', function() {
+        hmongspellchecker.check('Chicken', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "I"', function () {
-        hmongspellchecker.check('I', function (result) {
+    it('invalid word - "Wings"', function() {
+        hmongspellchecker.check('Wings', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "LoVe"', function () {
-        hmongspellchecker.check('LoVe', function (result) {
+    it('invalid word - "I"', function() {
+        hmongspellchecker.check('I', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "march"', function () {
-        hmongspellchecker.check('march', function (result) {
+    it('invalid word - "LoVe"', function() {
+        hmongspellchecker.check('LoVe', function(result) {
             result.should.equal(false);
         });
     });
 
-    it('invalid word - "B1rthD4y5"', function () {
-        hmongspellchecker.check('B1rthD4y5', function (result) {
+    it('invalid word - "march"', function() {
+        hmongspellchecker.check('march', function(result) {
+            result.should.equal(false);
+        });
+    });
+
+    it('invalid word - "B1rthD4y5"', function() {
+        hmongspellchecker.check('B1rthD4y5', function(result) {
             result.should.equal(false);
         });
     });
 });
 
-describe('Valid Hmong words', function () {
-    describe('hmong white words', function () {
-        it('valid word - "kooj"', function () {
-            hmongspellchecker.check('kooj', function (result) {
+describe('Valid Hmong words', function() {
+    describe('hmong white words', function() {
+        it('valid word - "kooj"', function() {
+            hmongspellchecker.check('kooj', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word - "tis"', function () {
-            hmongspellchecker.check('tis', function (result) {
+        it('valid word - "tis"', function() {
+            hmongspellchecker.check('tis', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word - "qaib"', function () {
-            hmongspellchecker.check('qaib', function (result) {
+        it('valid word - "qaib"', function() {
+            hmongspellchecker.check('qaib', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word (all caps) - "LIAB"', function () {
-            hmongspellchecker.check('LIAB', function (result) {
+        it('valid word (all caps) - "LIAB"', function() {
+            hmongspellchecker.check('LIAB', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word (mixed capitalization) - "nTsUaB"', function () {
-            hmongspellchecker.check('nTsUaB', function (result) {
+        it('valid word (mixed capitalization) - "nTsUaB"', function() {
+            hmongspellchecker.check('nTsUaB', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word (no tone maker) - "pw"', function () {
-            hmongspellchecker.check('pw', function (result) {
+        it('valid word (no tone maker) - "pw"', function() {
+            hmongspellchecker.check('pw', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word (no consonant) - "aws"', function () {
-            hmongspellchecker.check('aws', function (result) {
+        it('valid word (no consonant) - "aws"', function() {
+            hmongspellchecker.check('aws', function(result) {
                 result.should.equal(true);
             });
         });
     });
 
-    describe('hmong green words', function () {
-        it('valid word - "dlawb"', function () {
-            hmongspellchecker.check('dlawb', function (result) {
+    describe('hmong green words', function() {
+        it('valid word - "NOM"', function() {
+            hmongspellchecker.check('NOM', function(result) {
                 result.should.equal(true);
             });
         });
 
-        it('valid word - "DLEV"', function () {
-            hmongspellchecker.check('DLEV', function (result) {
+        it('valid word - "tsuv"', function() {
+            hmongspellchecker.check('tsuv', function(result) {
+                result.should.equal(true);
+            });
+        });
+
+        it('valid word - "tUm"', function() {
+            hmongspellchecker.check('tUm', function(result) {
                 result.should.equal(true);
             });
         });
